da601c939ae7b110b4e8f45fa0ef242c5a6f0c07
Kevin <kevin@Tardis.local>
2014-10-12 11:10:11 -0600

    new microphone
    can do either real audio input using pyaudio or fake microphone by
    keyboard

--- SoundServer.py
+++ SoundServer.py
@@ -33,30 +33,159 @@ CONTENT_TYPE = 'raw;encoding=signed-integer;bits=16;rate={0};endian={1}'.format(
 ###################################################################################
 
 class Microphone:
-	def __init__(self,real=True):
+	CHUNK = 1024
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 1
+	RATE = 44100
+	RECORD_SECONDS = 3
+
+	def __init__(self,wit_token,real=True):
+		self.real = real
+		
+		logging.basicConfig(level=logging.INFO)
+		self.logger = logging.getLogger(__name__)
+		
+		self.wit = wit.Wit(wit_token)
+
+		# get microphone	
+		if real: 
+			self.mic = pyaudio.PyAudio()
+	
+	def __del__(self):
+		if self.real: self.mic.terminate()
+		
+	"""
+	Will grab audio from microphone or text from keyboard
+	in: nothing
+	out: wit.ai json responce
+	"""
+	def stt(self):
+		txt = ''
+		ret = False
+
+		if self.real:
+			result = self.wit.post_speech(self.grabAudio( self.mic, 2 ), content_type=CONTENT_TYPE)
+			ans = self.getKey(result)
+		
+			# this doesn't work so good :(
+			if ans == 'attention':
+				self.logger.info('[*] Listening')
+				misc.playWave('sounds/misc/beep_hi.wav')
+			
+				txt = self.wit.post_speech(self.grabAudio( self.mic, 3 ), content_type=CONTENT_TYPE)
+			
+				misc.playWave('sounds/misc/beep_lo.wav')
+				self.logger.info('[*] Done listening')
+				ret = True
+				
+		else:
+			input = raw_input("YOU: ")
+			txt = self.wit.get_message(input)
+			ret = True
+			
+		return txt, ret
+		
+	"""
+	generator -- don't change!!
+	in: pyaudio and number of seconds to record
+	out: audio stream
+	"""
+	def grabAudio(self,p,sec):
+	
+		stream = p.open(
+			format=self.FORMAT, 
+			channels=self.CHANNELS, 
+			rate=self.RATE,
+			input=True, 
+			frames_per_buffer=self.CHUNK)
+	
+		for i in range(0, int(self.RATE / self.CHUNK * sec)): 
+			yield stream.read(self.CHUNK)
+	
+		stream.stop_stream()
+		stream.close()
+
+
+	
+	"""
+	Gets intent from msg and handles errors
+	in: wit.ai message
+	out: key (intent)
+	"""
+	def getKey(self,msg):
+		#print msg
+	
+		# hangle errors ----------------------------
+		if not msg:
+			self.logger.debug('<< no msg >>')
+			key = 'empty'
+		elif msg['msg_body'] == '':
+			self.logger.debug('<< no msg body >>')
+			key = 'empty'
+		elif 'outcome' not in msg:
+			self.logger.debug('no outcome')
+			key = 'error'
+		elif 'confidence' not in msg['outcome']:
+			self.logger.debug('no confidence')
+			key = 'error'
+		elif msg['outcome']['confidence'] < 0.5:
+			key = 'error'
+			print 'confidence',msg['outcome']['confidence']
+		else:
+			key = msg['outcome']['intent']
+	
+		return key
+
+# 	def calcThreshold(self):
+#  		# prepare recording stream
+# 		stream = self._audio.open(format=pyaudio.paInt16,
+# 								  channels=1,
+# 								  rate=RATE,
+# 								  input=True,
+# 								  frames_per_buffer=CHUNK)
+# 
+# 		# stores the audio data
+# 		frames = []
+# 
+# 		# stores the lastN score values
+# 		lastN = [i for i in range(30)]
+# 
+# 		# calculate the long run average, and thereby the proper threshold
+# 		for i in range(0, RATE / CHUNK * THRESHOLD_TIME):
+# 
+# 			data = stream.read(CHUNK)
+# 			frames.append(data)
+# 
+# 			# save this data point as a score
+# 			lastN.pop(0)
+# 			lastN.append(self.getScore(data))
+# 			average = sum(lastN) / len(lastN)
+# 
+# 		# this will be the benchmark to cause a disturbance over!
+# 		THRESHOLD = average * THRESHOLD_MULTIPLIER
 
 
 ###################################################################################
 
-"""
-generator -- don't change!!
-in: pyaudio and number of seconds to record
-out: audio stream
-"""
-def listen(p,sec):
-	
-	stream = p.open(
-		format=FORMAT, 
-		channels=CHANNELS, 
-		rate=RATE,
-		input=True, 
-		frames_per_buffer=CHUNK)
-	
-	for i in range(0, int(RATE / CHUNK * sec)): 
-		yield stream.read(CHUNK)
-	
-	stream.stop_stream()
-	stream.close()
+# """
+# generator -- don't change!!
+# in: pyaudio and number of seconds to record
+# out: audio stream
+# """
+# def listen(p,sec):
+# 	
+# 	stream = p.open(
+# 		format=FORMAT, 
+# 		channels=CHANNELS, 
+# 		rate=RATE,
+# 		input=True, 
+# 		frames_per_buffer=CHUNK)
+# 	
+# 	for i in range(0, int(RATE / CHUNK * sec)): 
+# 		yield stream.read(CHUNK)
+# 	
+# 	stream.stop_stream()
+# 	stream.close()
 	
 	
 
@@ -77,25 +206,15 @@ class SoundServer(mp.Process):
 		
 		# setup WIT.ai
 		wit_token = self.info['WIT_TOKEN']
-		self.logger.debug('Wit.ai API token %s'%(wit_token))
 		
 		if wit_token is None:
 			self.logger.info( 'Need Wit.ai token, exiting now ...' )
 			exit()	
-		self.wit = wit.Wit(wit_token)
+		else:
+			self.logger.info('Wit.ai API token %s'%(wit_token))
 		
 		# get microphone	
-		self.mic = pyaudio.PyAudio()
-		
-		# modules to handle different intents
-		#news = NewsModule()
-		#weather = WeatherModule()
-		#r = RandomModule()
-		#t = TimeModule()
-		#d = DateModule()
-		#e = ExitModule()
-		#starwars = StarWarsModule()
-		#self.modules = [news,weather,r,t,d,e,starwars]
+		self.mic = Microphone(wit_token,False)
 		
 		# Grab plugins
 		path = "plugins/"
@@ -148,20 +267,10 @@ class SoundServer(mp.Process):
 	def run(self):
 		# main loop
 		run = True
-		while run:
-			result = self.wit.post_speech(listen( self.mic, 2 ), content_type=CONTENT_TYPE)
-			ans = self.getKey(result)
-			
-			# this doesn't work so good :(
-			if ans == 'attention':
-				self.logger.info('[*] Listening')
-				misc.playWave('sounds/misc/beep_hi.wav')
-				
-				result = self.wit.post_speech(listen( self.mic, 3 ), content_type=CONTENT_TYPE)
-				
-				misc.playWave('sounds/misc/beep_lo.wav')
-				self.logger.info('[*] Done listening')
-				txt = self.handleVoice(result)
+		while run:			
+			result,ret = self.mic.stt()
+			if ret:
+ 				txt = self.handleVoice(result)
 				
 				if txt == 'exit_loop':
 					run = False
@@ -169,13 +278,13 @@ class SoundServer(mp.Process):
 					self.playTxt(txt)
 		
 		self.playTxt('Good bye ...')
-		self.mic.terminate()
 		
 	
 	"""
 	Gets intent from msg and handles errors
 	in: wit.ai message
 	out: key (intent)
+	todo: duplicates of this
 	"""
 	def getKey(self,msg):
 		#print msg
