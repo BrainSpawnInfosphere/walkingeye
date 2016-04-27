07bb1cfa6aac17abd6c422574a5c3f77d0811e61
Kevin <walchko@users.noreply.github.com>
2016-04-27 16:11:55 -0600

    Added files via upload

---
+++ test/test2.py
@@ -0,0 +1,45 @@
+
+import UserDict
+import json
+import pprint as pp
+
+
+# Everything here is a dictionary ... i think it might make things easier
+# Bob() -> dict -> json -> ZeroMQ ->de-json -> dict
+
+# class Bob(UserDict.UserDict):
+class Bob(dict):
+    # __slots__ = ['x','y','z']
+    # def __init__(self, data = {}, **kw):
+    def __init__(self,**kw):
+        # UserDict.UserDict.__init__(self)
+        dict.__init__(self)
+        # if True:
+        default = {'x':0.0, 'y':0.0, 'z':0.0}
+        self.update(default)
+        if kw: self.update(kw)
+        # self.update(m='5')
+        # print 'data',data
+        # print 'kw',kw
+        # self.update(x=1)
+        # self.update(y=2)
+        # self.update(z=3)
+
+class Tom(object):
+    def __init__(self):
+        self.x = 1
+        self.lin = Bob(x=500.0, z=-234.1)
+        self.ang = Bob()
+
+t = Tom()
+
+d = vars(t)
+
+pp.pprint(d)
+print 'x',d['lin']['x']
+
+d['lin']['x']=5
+print 'new x',d['lin']['x']
+
+# print 'serialize',t.serialize()
+pp.pprint( json.dumps(t, default=lambda o: vars(o)) )
