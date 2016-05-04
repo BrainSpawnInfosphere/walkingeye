244447545345332b5f3e21b70b9fb7a0c8fa3d94
walchko <kevin.walchko@outlook.com>
2016-05-03 22:13:59 -0600

    nav update

--- navigation.py
+++ navigation.py
@@ -13,24 +13,50 @@ import datetime as dt
 import cv2
 import argparse
 import numpy as np
+import math
 
+def ecef(lat,lon,H):
+	# phi = lat
+	# lambda = lon
+	e = 1
+	re = 6378137.0 # m
+	rm = re*(1.0-e**2)/math.pow(1.0-e**2*math.sin(lat)**2,3.0/2.0)
+	rn = re/math.sqrt(1.0-e**2*math.sin(lat)**2)
+	x = (rn+H)*math.cos(lat)math.cos(lon)
+	y = (rn+H)*math.cos(lat)*math.sin(lon)
+	z = (rm+H)*math.sin(lat)
+	return x,y,z
 
 def fillMatrix(a,b,m,n,i=0,j=0):
 	"""
 	Set a(i:m,j:n) = b
 	Why numpy doesn't support something this useful already I don't know ... idiots!
 	"""
+	# j=j-1
+	# i=i-1
 	for y in range(j,j+n):
 		for x in range(i,i+m):
 			a[x,y] = b[x-i,y-j]
-	
+			# print x,y,a[x,y]
 
-wie = 5
+def updateQ(wx,wy,wz):
+	Q = 0.5*np.array([(0,wz,-wy,wx),
+	          (-wz,0,wz,-wy),
+			    (wy,-wx,0,wz),
+				 (-wx,-wy,-wz,0)])
+	return Q
+
+wie = 7.292115E-15          # earth rotation
 oe_ie = np.array([(0,-wie,0),(wie,0,0),(0,0,0)])
-A = np.arange(9*9).reshape(9,9)
+Q = updateQ(10,10,10)       # Attitude
+A = np.zeros([10,10])       # state transition matrix
 fillMatrix(A,oe_ie,3,3)
-fillMatrix(A,-oe_ie*oe_ie,3,3,3,0)
-fillMatrix(A,np.eye(3,3),3,3,?,?)
+fillMatrix(A,-oe_ie*oe_ie,3,3,0,3)
+fillMatrix(A,np.eye(3,3),3,3,3,0)
+fillMatrix(A,Q,4,4,6,6)
+nQ = np.diag([1,2,3,4,5,6]) # process noise
+nR = np.diag([1,2,3,4,5,6]) # measurement noise
+g = np.array([(0,0,9.78,0,0,0)]) # gravity model
 
 class Navigation(mp.Process):
 	def __init__(self,host="localhost",port='9110'):
@@ -43,9 +69,9 @@ class Navigation(mp.Process):
 		self.logger = logging.getLogger('navigation')
 
 		self.kalman = cv2.KalmanFilter(4,2)
-		self.kalman.measurementMatrix = np.array([[1,0,0,0],[0,1,0,0]],np.float32)
-		self.kalman.transitionMatrix = np.array([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],np.float32)
-		self.kalman.processNoiseCov = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],np.float32) * 0.03
+		self.kalman.measurementMatrix = C #np.array([[1,0,0,0],[0,1,0,0]],np.float32)
+		self.kalman.transitionMatrix = A #np.array([[1,0,1,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],np.float32)
+		self.kalman.processNoiseCov = nQ #np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],np.float32) * 0.03
 
 		# self.epoch = dt.datetime.now()
 	def process(self,z):
@@ -65,10 +91,17 @@ class Navigation(mp.Process):
 				msg = self.sub.recv()
 				if msg:
 					x = self.process()
-					
+
 					pub.pub(x)
 				time.sleep(0.05)
 
 
 		except KeyboardInterrupt:
 			pass
+
+def main():
+	print A
+
+
+if __name__ == '__main__':
+	main()
