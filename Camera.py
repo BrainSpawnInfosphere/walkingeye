5eca88dde31e2dfc641e500b6a7ae387e4cb7788
Kevin <kevin.walchko@gmail.com>
2016-05-16 08:45:48 -0600

    creating standard camera file

--- Camera.py
+++ Camera.py
@@ -1,248 +1,126 @@
 #!/usr/bin/env python
-#
-# Kevin J. Walchko 11 Nov 2014
-#
-
-import sys
-import time
-import json
-from zmqclass import *
-import multiprocessing as mp
-import logging
-import datetime as dt
-import cv2
-import argparse
-
-####################################################################
-# RobotCameraServer streams camera images as fast as
-# possible.
-####################################################################
-class RobotCameraServer(mp.Process):
-	def __init__(self,host="localhost",port='9100',camera_num=0):
-		mp.Process.__init__(self)
-		self.epoch = dt.datetime.now()
-		self.host = host
-		self.port = port
-		self.camera_num = camera_num
-		logging.basicConfig(level=logging.INFO)
-		self.logger = logging.getLogger('robot')
-
-		self.epoch = dt.datetime.now()
-
-	def run(self):
-		self.logger.info(str(self.name)+'['+str(self.pid)+'] started on'+ str(self.host) + ':' + str(self.port) +', Daemon: '+str(self.daemon))
-
-		pub = PubBase64('tcp://'+self.host+':'+self.port)
-		camera = cv2.VideoCapture(self.camera_num)
-		self.logger.info('Openned camera: '+str(self.camera_num))
-
-		try:
-			while True:
-				ret, frame = camera.read()
-				jpeg = cv2.imencode('.jpg',frame)[1]
-				pub.pub('image',jpeg)
-				#print '[*] frame: %d k   jpeg: %d k'%(frame.size/1000,len(jpeg)/1000)
-				#time.sleep(0.1)
-
-		except KeyboardInterrupt:
-			pass
 
 
-class SaveVideo(object):
+import cv2             # OpenCV camera
+import time            # sleep
+import logging         # logging
+import platform        # determine linux or darwin (OSX)
+import argparse        # command line args
+
+
+if platform.system().lower() == 'linux':
+	import picamera       # on linux, PiCamera
+	import picamera.array # on linux, turn PiCamera images into numpy arrays
+
+
+class Camera(object):
 	"""
-	Simple class to save frames to video (mp4v)
+	Generic camera object that can switch between OpenCv in PiCamera.
 	"""
-	def __init__(self,filename,image_size,fps=20):
-		mpg4 = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-		self.out = cv2.VideoWriter()
-		self.out.open(filename,mpg4,fps, image_size)
+	def __init__(self, cam=None, num=0):
+		"""
+		Constructor
+		Sets up the camera either for OpenCV camera or PiCamera. If nothing is
+		passed in, then it determines the operating system and picks which
+		camera to use.
+		default:
+		    linux: PiCamera
+			OSX: OpenCV
+		in: type: cv or pi and if OpenCV, what camera number
+		out: None
+		"""
+		self.cal = None
 
-	def write(self,image):
-		self.out.write(image)
+		if not cam:
+			os = platform.system().lower() # grab OS name and make lower case
+			if os == 'linux': cam = 'pi'
+			else: cam = 'cv'
 
-	def release(self):
-		self.out.release()
+		if cam == 'pi':
+			self.cameraType = 'pi' # picamera
+			self.camera = picamera.PiCamera()
+		else:
+			self.cameraType = 'cv' # opencv
+			self.cameraNumber = num
+			self.camera = cv2.VideoCapture()
+			# need to do vertical flip?
 
-class CameraSaveClient(object):
-	"""
-	"""
-	def __init__(self,host,port,filename):
-		self.host = host
-		self.port = port
-		self.save = SaveVideo(filename,(640,480)) # need to fix image size
+		time.sleep(1) # let camera warm-up
 
-	def run(self,save):
+	def __del__(self):
+		"""
+		Destructor
+		Closes and turns off the camera on exit (when this class goes out of scope).
+		"""
+		# the red light should shut off
+		if self.cameraType == 'pi': self.camera.close()
+		else: self.camera.release()
 
-		sub_topics = ['image']
+		print 'exiting camera ... bye!'
 
-		p = SubBase64(sub_topics,'tcp://'+self.host+':'+self.port)
+	def init(self,win=(640,480)):
+		"""
+		Initialize the camera and set the image size
+		in: image size (tuple (width,height))
+		out: None
+		"""
+		if self.cameraType == 'pi':
+			self.camera.vflip = True # camera is mounted upside down
+			self.camera.resolution = win
+			self.bgr = picamera.array.PiRGBArray(self.camera,size=win)
+		else:
+			self.camera.open(self.cameraNumber)
+			self.camera.set(3, win[0]);
+			self.camera.set(4, win[1]);
+		# self.capture.set(cv2.cv.CV_CAP_PROP_SATURATION,0.2);
 
-		try:
-			while True:
-				topic,msg = p.recv()
+	def setCalibration(self,n):
+		"""
+		Set the calibration data for the camera
+		in: numpy array of calibration data
+		out: None
+		"""
+		self.cal = n
 
-				if not msg:
-					pass
-				elif 'image' in msg:
-					im = msg['image']
-					buf = cv2.imdecode(im,1)
+	def read(self):
+		"""
+		Reads a gray scale image
+		in: None
+		out: cv image (numpy array) in grayscale
+		"""
+		gray = 0
 
-					self.save.write(buf)
+		if self.cameraType == 'pi':
+			self.camera.capture(self.bgr, format='bgr', use_video_port=True)
+			gray = cv2.cvtColor(self.bgr.array, cv2.COLOR_BGR2GRAY)
+			self.bgr.truncate(0) # clear stream
+			# print 'got image'
+			# return True, gray
+		else:
+			ret,img = self.camera.read()
+			if not ret:
+				return False
+			# imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			# return True, gray
 
-		except KeyboardInterrupt:
-			self.save.release()
-			pass
+		if self.cal: # FIXME 2016-05-15
+			print 'do calibration correction ... not done yet'
 
+		return True, gray
 
-class ImageWriter(object):
-	"""
-	value here?
-	"""
-	def __init__(self):
-		font = cv2.FONT_HERSHEY_SIMPLEX
-		font_scale = 1
-		font_color = (155,0,0)
-
-	def writeMsg(frame,msg):
-		# font = cv2.FONT_HERSHEY_SIMPLEX
-		# font_scale = 1
-		# font_color = (155,0,0)
-		cv2.putText(frame, msg,(100,100),self.font,self.font_scale,self.font_color,2)
-
-class LocalCamera(object):
-	def __init__(self,args):
-		if args['window']: size = args['window']
-		else: size = [640,480]
-
-		if args['file']: self.save = args['file']
-		else: self.save = 'video.mp4'
-
-		self.width = int(size[0])
-		self.height = int(size[1])
-		self.camera = int(args['camera'])
-
-	def run(self):
-
-		# Source: 0 - built in camera  1 - USB attached camera
-		cap = cv2.VideoCapture(self.camera)
-
-		ret = cap.set(3,self.width)
-		ret = cap.set(4,self.height)
-
-		# ret, frame = cap.read()
-		# h,w,d = frame.shape
-
-		# create a video writer to same images
-		sv = 0
-
-		print 'Press q - quit   SPACE - grab image  s - save video'
-
-		save = False
-
-		while(True):
-			# Capture frame-by-frame
-			ret, frame = cap.read()
-
-			if ret == True:
-
-				# Display the resulting frame
-				cv2.imshow('frame',frame)
-
-				if save:
-					if sv == 0: sv = SaveVideo(self.save,(self.width,self.height))
-					sv.write(frame)
-
-			key = cv2.waitKey(10)
-			if key == ord('q'):
-				break
-			elif key == ord(' '):
-				print 'Grabbing picture'
-			elif key == ord('s'):
-				save = not save
-				print 'Saving video: ' + str(save)
-
-		# When everything done, release the capture
-		cap.release()
-		sv.release()
-		cv2.destroyAllWindows()
-
-class CameraDisplayClient(object):
-	"""
-	"""
-	def __init__(self,host,port):
-		self.host = host
-		self.port = port
-		self.save = False
-
-	def run(self):
-
-		sub_topics = ['image']
-
-		p = SubBase64(sub_topics,'tcp://'+self.host+':'+self.port)
-
-		try:
-			while True:
-				topic,msg = p.recv()
-
-				if not msg:
-					pass
-				elif 'image' in msg:
-					im = msg['image']
-					buf = cv2.imdecode(im,1)
-					cv2.imshow('image',buf)
-					cv2.waitKey(10)
-
-		except KeyboardInterrupt:
-			pass
+	def isOpen(self):
+		"""
+		Determines if the camera is opened or not
+		in: None
+		out: True/False
+		"""
+		return True # FIXME 2016-05-15
 
 
-# set up and handle command line args
-def handleArgs():
-	parser = argparse.ArgumentParser(description='A simple zero MQ pub/sub for a camera Example: RobotCameraServer pub 192.168.10.22 8080')
-	# parser.add_argument('info', nargs=3, help='pub or sub, hostname, port; example: pub 10.1.1.1 9333', default=['pub','localhost','9000'])
-	# parser.add_argument('-a', '--address', help='host address', default='localhost')
-	# parser.add_argument('-p', '--port', help='port', default='9100')
-	parser.add_argument('-f', '--file', help='file name to save video to')
-	# parser.add_argument('-g', '--host', nargs=2, help='size of pattern, for example, -s 6 7', required=True)
-	# parser.add_argument('-p', '--path', help='location of images to use', required=True)
-	# parser.add_argument('-d', '--display', help='display images', default=True)
-	parser.add_argument('-p', '--pub', nargs=2, help='publish images to addr:port, ex. pub 10.1.1.1 9000')
-	parser.add_argument('-s', '--sub', nargs=2, help='subscribe to images at addr:port, ex. sub 10.1.1.1 9000')
-	parser.add_argument('-l', '--local', help='display images to screen', action='store_true')
-	parser.add_argument('-w', '--window', nargs=2, help='set window size, ex. -w 640 480')
-	parser.add_argument('-c', '--camera', help='set camera number, ex. -c 1', type=int, default=0)
-	# parser.add_argument('-f', '--file', help='if local, save images to file')
-
-	args = vars(parser.parse_args())
-	return args
 
 def main():
-	"""
-	need to figure out how to cleanly handle window size, save to file for everything
-
-	maybe pass a dict to every class and let it figure it out instead of function args
-	"""
-	args = handleArgs()
-
-	print args
-	# exit()
-
-	if args['sub']:
-		sub = 0
-		if args['file']: sub = CameraSaveClient(args['file'])
-		else: sub = CameraDisplayClient(args['sub'][0],args['sub'][1])
-		sub.run()
-
-	elif args['pub']:
-		pub = RobotCameraServer(args['pub'][0],args['pub'][1],args['camera'])
-		pub.run()
-
-	elif args['local']:
-		local = LocalCamera(args)
-		local.run()
-	else:
-		print 'Error'
-
+	print 'Hello!'
 
 if __name__ == "__main__":
 	main()
