a3b468da8a3543fed72dc4e86c55cb9a679c229b
walchko <kevin.walchko@outlook.com>
2016-07-02 15:39:36 -0600

    servos working?

--- tmp/servo/ServoTune.py
+++ tmp/servo/ServoTune.py
@@ -1,29 +1,28 @@
+#!/usr/bin/env python
+
+# from __future__ import print_function
+# from __future__ import division
 from __future__ import print_function
 from __future__ import division
 import time
 from Adafruit_PCA9685 import PCA9685
-
-
-#!/usr/bin env python
-from __future__ import print_function
-from __future__ import division
 import math
 
 
 class Servo(object):
 	"""
-	Keeps info for servo.
+	Keeps info for servo. This only holds angle info.
 	"""
 	def __init__(self, pin, pos0, rate, limits=None):
 		"""
 		pin [int ]- pin number the servo is attached too
 		pos0 [angle] - initial or neutral position
 		rate - ???
-		limits [angle, angle] - [optional] set the angular limits of the servo
+		limits [angle, angle] - [optional] set the angular limits of the servo to avoid collision
 		"""
 		self.pos0 = pos0
-		self.rate = rate
-		self.pin = pin
+# 		self.rate = rate  # why?
+# 		self.pin = pin  # why?
 
 		if limits:
 			self.setServoLimits(*limits)
@@ -31,7 +30,6 @@ class Servo(object):
 			self.maxAngle = 90
 			self.minAngle = -90
 
-		# self.serial = serial
 		self.angle = 0
 
 	def clamp(self, angle):
@@ -58,14 +56,6 @@ class Servo(object):
 		"""
 		self.angle = self.pos0
 
-	# def getServoAngle(self):
-	# 	"""
-	# 	Return the current commanded servo angle
-	# 	in: None
-	# 	out: servo angle [degrees]
-	# 	"""
-	# 	return self.angle
-
 	def moveToAngle(self, angle):
 		"""
 		Moves the sevo to desired angle
@@ -77,30 +67,37 @@ class Servo(object):
 		# clamp to limits
 		newAngle = clamp(angle, self.minAngle, self.maxAngle)
 
-		if newAngle != self.angle:
-			self.angle = newAngle
+		self.angle = newAngle
 
 
 class ServoController(object):
+	"""
+	Tried to optimize pwm params for TG9e servos.
+	TG9e = [130, 655]
+	"""
 	servos = []
-	pwm_max = 600  # Max pulse length out of 4096
-	pwm_min = 200  # Min pulse length out of 4096
+	pwm_max = 655  # Max pulse length out of 4096
+	pwm_min = 130  # Min pulse length out of 4096
 	minAngle = -90  # not sure the right way to do this!
 	maxAngle = 90
 
 	def __init__(self, freq=60):
 		self.pwm = PCA9685()
 		self.pwm.set_pwm_freq(freq)
-		for i in range(0, 16): self.servos[i] = Servo(i, 0, 0)
+		for i in range(0, 16): self.servos.append(Servo(i, 0, 0))
 
-	def moveAllServos(self):
+	def moveAllServos(self, angle=None):
 		for i, servo in enumerate(self.servos):
-			pulse = self.angleToPWM(servo.angle, servo.minAngle, servo.maxAngle)
+			if angle is None:
+				angle = servo.angle
+			pulse = self.angleToPWM(angle, servo.minAngle, servo.maxAngle)
 			self.pwm.set_pwm(i, 0, pulse)
 
-	def moveServo(self, i):
+	def moveServo(self, i, angle=None):
+		if angle is None:
+			angle = servo.angle
 		servo = self.servos[i]
-		pulse = self.angleToPWM(servo.angle, servo.minAngle, servo.maxAngle)
+		pulse = self.angleToPWM(angle, servo.minAngle, servo.maxAngle)
 		self.pwm.set_pwm(i, 0, pulse)
 
 	def angleToPWM(self, angle, mina, maxa):
@@ -120,11 +117,17 @@ class ServoController(object):
 		pulse = m * angle + b  # y=m*x+b
 		return int(pulse)
 
-	def allStop(self):
-		self.pwm.set_all_pwm(0,0x1010)
-
-def testRange(servo):
-	sc = ServoController()
+	def allStop(self):  # FIXME: 20160702 can i stop individual servos too?
+		self.pwm.set_all_pwm(0,0x1000)
+		
+	def checkPwmRange(self, channel):
+		self.allStop()
+		for i in range(0, 700, 10):
+			print('pos: {}'.format(i))
+			self.pwm.set_pwm(channel, 0, i)
+			time.sleep(1)
+			# towerpro 100-660
+			# tg9e 130-650
 
 def handleArgs():
 	parser = argparse.ArgumentParser(description='A simple zero MQ publisher for joystick messages')
@@ -133,29 +136,17 @@ def handleArgs():
 	parser.add_argument('-v', '--verbose', help='display info to screen', action='store_true')
 	args = vars(parser.parse_args())
 	return args
-
-# channel = 3
-# pwm = PCA9685()
-# pwm.set_pwm_freq(60)  # not sure why ... should be 50 Hz
-#
-# pwm.set_pwm(channel, 0, 150)
-# time.sleep(3)
-# pwm.set_pwm(channel, 0, 600)
-# time.sleep(3)
-#
-# angle = -90.0
-#
-# while angle < 90.0:
-# 	pulse = angleToPWM(angle, -90, 90)
-# 	pwm.set_pwm(channel, 0, pulse)
-# 	time.sleep(0.1)  # 50 Hz update rate
-# 	angle += 5.0
+	
 
 def main():
-	args = handleArgs()
-	mins, maxs = args['limits']
-	pwm = PCA9685()
-	pwm.set_pwm_freq(60)
-
+	sc = ServoController()
+	sc.allStop()
+# 	sc.test(15)
+	for angle in range(-90,90,10): 
+		sc.moveServo(15, angle)
+		time.sleep(1)
+	sc.allStop()
+	
+	
 if __name__ == "__main__":
 	main()
