e6e49b3974255927f3692f649d68d1a6b0383ff0
Kevin <kevin.walchko@outlook.com>
2016-12-04 16:30:13 -0700

    Clean up

--- pygecko/Robot.py
+++ pygecko/Robot.py
@@ -8,14 +8,17 @@
 from __future__ import print_function
 from __future__ import division
 import time
+# import numpy as np
+# from math import radians as d2r
 from math import pi
+from pygecko.lib.ZmqClass import Sub as zmqSub
 from Quadruped import Quadruped
 from Gait import DiscreteRippleGait
 
 ##########################
 
 
-class SimpleQuadruped(Quadruped):
+class pyGeckoQuadruped(Quadruped):
 	def __init__(self, data):
 		Quadruped.__init__(self, data)
 
@@ -23,44 +26,39 @@ class SimpleQuadruped(Quadruped):
 		leg = self.robot.legs[0].foot0
 		self.crawl = DiscreteRippleGait(45.0, leg)
 		# self.crawl = ContinousRippleGait(5.0, leg)
-		# self.crawl.alpha = 0.5
-		self.path = [  # x,y,rot
-			[50, 0, 0],
-			[50, 0, 0],
-			[50, 0, 0],
-			[50, 0, 0],
-			[50, 0, 0],
-			[0, 0, pi/4],
-			[0, 0, pi/4],
-			[0, 0, pi/4],
-			[0, 0, -pi/4],
-			[0, 0, -pi/4],
-			[0, 0, -pi/4],
-			[-50, 0, 0],
-			[-50, 0, 0],
-			[-50, 0, 0],
-			[-50, 0, 0],
-			[-50, 0, 0],
-		]
+
 
 	def run(self):
-		# run = True
-		# while run:
-		for pose in self.path:
-			x, y, rz = pose
-			leg = self.robot.legs[0].foot0
-			cmd = [x, y, rz]
-			print('***********************************')
-			print('* rest {:.2f} {:.2f} {:.2f}'.format(*leg))
-			print('* cmd {:.2f} {:.2f} {:.2f}'.format(*cmd))
-			print('***********************************')
-			self.crawl.command(cmd, self.robot.moveFoot, steps=12)
+		sub = zmqSub('js', ('localhost', '9000'))
+
+		print('Press <share> on PS4 controller to exit')
+
+		while True:
+			topic, ps4 = sub.recv()
+
+			# msg values range between (-1, 1)
+			if ps4 and topic == 'js':
+				x, y = ps4['axes']['leftStick']
+				rz = ps4['axes']['rightStick'][1]
+
+				if ps4['buttons']['share']:
+					print('You hit <share> ... bye!')
+					exit()
+
+				cmd = [100*x, 100*y, 40*rz]
+				print('***********************************')
+				print('* xyz {:.2f} {:.2f} {:.2f} *'.format(x, y, rz))
+				print('* cmd {:.2f} {:.2f} {:.2f} *'.format(*cmd))
+				print('***********************************')
+				self.crawl.command(cmd, self.robot.moveFoot)
+			time.sleep(0.01)
 
 
 def run():
 	# angles are always [min, max]
 	# xl-320
 	test = {
+		# 'serialPort': '/dev/tty.usbserial-A5004Flb',  # original debug
 		'serialPort': '/dev/tty.usbserial-A700h2xE',  # robot
 		'legLengths': {
 			'coxaLength': 45,
@@ -71,7 +69,7 @@ def run():
 		'legOffset': [150, 150, 150+90]
 	}
 
-	robot = SimpleQuadruped(test)
+	robot = pyGeckoQuadruped(test)
 	robot.run()
 
 
